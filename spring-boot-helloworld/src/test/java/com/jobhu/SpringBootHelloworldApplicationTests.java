f19ff72ce180fef2de09c284b5b3139b075c7d32
Administrator <Administrator@WIN-9MV2KV5D5P2>
2019-12-29 09:12:17 +0800

    测试

--- spring-boot-helloworld/src/test/java/com/jobhu/SpringBootHelloworldApplicationTests.java
+++ spring-boot-helloworld/src/test/java/com/jobhu/SpringBootHelloworldApplicationTests.java
@@ -1,13 +1,24 @@
 package com.jobhu;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.jobhu.pojo.Person;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootHelloworldApplicationTests {
 
+
+	@Autowired
+	Person person;
+	
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
