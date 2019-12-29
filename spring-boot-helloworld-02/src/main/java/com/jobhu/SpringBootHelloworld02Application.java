ee76dc4a094b163c437aa0336b098ce23d989ace
Administrator <Administrator@WIN-9MV2KV5D5P2>
2019-12-29 15:14:15 +0800

    springboot测试

---
+++ spring-boot-helloworld-02/src/main/java/com/jobhu/SpringBootHelloworld02Application.java
@@ -0,0 +1,13 @@
+package com.jobhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworld02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworld02Application.class, args);
+	}
+
+}
---
+++ spring-boot-helloworld-02/src/test/java/com/jobhu/SpringBootHelloworld02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworld02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
